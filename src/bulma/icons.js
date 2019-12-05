a8a6abcd78620f4a9587b48cb21a3c72b0e736fa
reza <raftx24@gmail.com>
2019-12-05 11:21:39 +0330

    first commit

---
+++ src/bulma/icons.js
@@ -0,0 +1,11 @@
+import '@measurement-units/icons';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {
+    faBuilding,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add([
+    faBuilding,
+]);
