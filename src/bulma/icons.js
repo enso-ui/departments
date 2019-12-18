2903be16ecec4e130d9a8b2f4704f3c4f23d0ba3
mishu <mishu@xtelecom.ro>
2019-12-18 09:09:21 +0200

    updates icon & deps

--- src/bulma/icons.js
+++ src/bulma/icons.js
@@ -2,10 +2,6 @@ import '@measurement-units/icons';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-import {
-    faBuilding,
-} from '@fortawesome/free-solid-svg-icons';
+import { faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
 
-library.add([
-    faBuilding,
-]);
+library.add(faProjectDiagram);
